27af56c9a92d322c13aefc7f4ca2ae9fe1e443c2
Pavel Myagkov <p.myagkov@corp.mail.ru>
2016-04-05 11:55:47 +0300

    sticky header

--- onestep/js/cart.js
+++ onestep/js/cart.js
@@ -27,14 +27,29 @@ $(function () {
 
   onShippingChange();
 
+  $.fn.scrollTo = function( target, options, callback ){
+    if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+    var settings = $.extend({
+      scrollTarget  : target,
+      offsetTop     : 50,
+      duration      : 500,
+      easing        : 'swing'
+    }, options);
+    return this.each(function(){
+      var scrollPane = $(this);
+      var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+      var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+      scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+        if (typeof callback == 'function') { callback.call(this); }
+      });
+    });
+  }
+
   $('.cart .cart-total__checkout').click(function () {
-    var top = $('.checkout').show().offset().top;
-    var $body = $("html, body");
+    $('.checkout').show();
+    $('body').scrollTo('.checkout', { offsetTop: '200' });
 
-    window.scrollTo(0, top - 20);
-
-
-
+    $('.onestep-cart').addClass('cart-embed');
 
     return false;
   });
